5de46aac8be2d838bb8393a59585a0b652838569
harish <109896595+Harish1am@users.noreply.github.com>
2023-06-09 10:48:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <body background="river.jpg">
     
     <div id="input">
-        <h2>Make your june 7 bucket list!</h2>
+        <h2>Make your june 9 bucket list!</h2>
     <form id="frm" >
     
     <input type="text" id="task"  name="task" placeholder="enter some check_list" class="task_input">
